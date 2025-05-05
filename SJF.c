f5cbf2135e4822c5675637722a65e9f9cd69e54b
Abdul Husain Dahodwala <117434840+Abdulhusa@users.noreply.github.com>
2025-05-05 19:34:41 +0530

    Update SJF.c
    removed the redundancy of the code

--- SJF.c
+++ SJF.c
@@ -9,13 +9,15 @@ struct sjf{
   int CT; //calculating completition time for processes
   int TAT; //calculating turnaround time for processes
   int WT; //calculating waiting time for each processes
+  int completed;
+  
 };
 
 void sort(struct sjf arr[],int n){
     struct sjf temp;
     for(int i = 0;i < n; i++){
         for(int j = i+1;j < n; j++){
-            if(arr[i].BT > arr[j].BT || (arr[i].BT == arr[j].BT && arr[i].AT > arr[j].AT)){
+            if(arr[i].AT > arr[j].AT){
                 temp = arr[i];
                 arr[i] = arr[j];
                 arr[j] = temp;
@@ -29,7 +31,8 @@ void main(){
     int atat = 0;
     int awt = 0;
     int n;
-
+    int completed = 0;
+    
     
     printf("Enter the Number of Processes : ");
     scanf("%d",&n);  //taking input for proccess numbers
@@ -38,6 +41,7 @@ void main(){
     //input for arrival and burst time and assigning process numbers to them
     for (int i = 0; i < n; i++) {
         arr[i].pno = i + 1; 
+        arr[i].completed = 0;
         printf("Enter arrival time for process %d: ", i + 1);
         scanf("%d", &arr[i].AT);
         printf("Enter burst time for process %d: ", i + 1);
@@ -46,22 +50,37 @@ void main(){
     
     sort(arr,n); //ssorting based on arrival time
     
-    for(int i = 0; i < n; i++){
-        if(temp < arr[i].AT){
-            printf("Idle Time of the CPU : %d -> %d",temp,arr[i].AT);
-            temp = arr[i].AT;
-            
+    
+    while(completed < n){
+        int shortIndex = -1;
+        int min_bt = 9999;
+        
+        for (int i = 0; i < n; i++){
+            if(!arr[i].completed && arr[i].AT <= temp && arr[i].BT <= min_bt){
+                shortIndex = i;
+                min_bt = arr[i].BT;
+            }
         }
-        arr[i].CT = temp + arr[i].BT; //completition time of the processes
-        arr[i].TAT = arr[i].CT - arr[i].AT; // turnaround timw of the processes
-        arr[i].WT = arr[i].TAT - arr[i].BT; //waiting time of the processes
         
-        atat += arr[i].TAT; //avaerage turnaround time
-        awt += arr[i].WT; //average waiting time
+        if(shortIndex == -1){
+            printf("\nIdle Time till %d ",temp);
+        }
+        else{
+            
+        arr[shortIndex].CT = temp + arr[shortIndex].BT; //completition time of the processes
+        arr[shortIndex].TAT = arr[shortIndex].CT - arr[shortIndex].AT; // turnaround timw of the processes
+        arr[shortIndex].WT = arr[shortIndex].TAT - arr[shortIndex].BT; //waiting time of the processes
         
-        temp = arr[i].CT; //updating temp for further idle scenarios
-                
+        atat += arr[shortIndex].TAT; //avaerage turnaround time
+        awt += arr[shortIndex].WT; //average waiting time
+        arr[shortIndex].completed = 1;
+        
+        temp = arr[shortIndex].CT; //updating temp for further idle scenarios
+        completed++;
+        }
     }
+                
+    
     
     printf("\nProcess No\tArrival Time\tBurst Time\tCompletion Time\tTAT\tWaiting Time\n");
     for (int i = 0; i < n; i++) {
